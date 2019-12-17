ed92d44bd327cede6a38a51be408f43f461ac094
Scott Hoisington <hoyzington@gmail.com>
2019-12-17 18:11:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+  def name=(n)
+    @dog_name = n
+  end
   
+  def name
+    @dog_name
+  end
+  
+  def breed=(b)
+    @dog_breed = b
+  end
+  
+  def breed
+    @dog_breed
+  end
 end
